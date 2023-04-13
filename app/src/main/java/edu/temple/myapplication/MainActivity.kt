f027b16fad67ab999613bb02480312e1a208e47d
Jordan Santoli <jds442@drexel.edu>
2023-04-13 11:37:05 -0400

    Bound service in MainActivity

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -10,20 +10,47 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+
+    lateinit var timerBinder:TimerService.TimerBinder
+    var isConnected = false
+    val serviceConnection = object:ServiceConnection{
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            timerBinder = p1 as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            isConnected = false
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.playButton).setOnClickListener {
+        bindService(Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
 
+        findViewById<Button>(R.id.playButton).setOnClickListener {
+            if(isConnected) timerBinder.start(100)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            if(isConnected) timerBinder.pause()
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            if(isConnected) timerBinder.stop()
         }
+
+
+    }
+
+    override fun onDestroy() {
+        unbindService(serviceConnection)
+        super.onDestroy()
     }
 }
